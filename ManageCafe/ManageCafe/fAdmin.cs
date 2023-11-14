53cf3c68bc86314c343334d49fce3b6053e5498f
KuroN3ko276 <nghaidang276@gmail.com>
2023-11-14 15:06:50 +0700

    create DataProvider
    tao class DataProvider

--- ManageCafe/ManageCafe/fAdmin.cs
+++ ManageCafe/ManageCafe/fAdmin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ManageCafe.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,8 +16,30 @@ namespace ManageCafe
         public fAdmin()
         {
             InitializeComponent();
+            LoadAccountList();
         }
 
-        
-    }
+        void LoadAccountList()
+        {
+            string query = "EXEC dbo.USP_GetAccountByUserName @userName";
+
+            DataProvider provider = new DataProvider();
+
+            dtgvAccount.DataSource = provider.ExecuteQuery(query, new object[] {"staff"});
+
+        }
+
+		//void LoadFoodCategory()
+		//{
+		//	string query = "select * from dbo.FoodCategory";
+
+		//	DataProvider provider = new DataProvider();
+
+		//	dtgvCategory.DataSource = provider.ExecuteQuery(query);
+
+		//}
+
+
+
+	}
 }
